12885acbef9688802069972c1cbd082f8fcdccea
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2016-05-04 13:37:04 +0200

    Added default SRDF to surpress self-collisions, and added collision feedback in service.

--- knowrob_moveit_core/scripts/call_collision_check.py
+++ knowrob_moveit_core/scripts/call_collision_check.py
@@ -11,22 +11,26 @@ def get_param_or_die(param_name):
         sys.exit(0) 
     return rospy.get_param(param_name)
 
-def joint_state_map_to_msg(js_map):
+def joint_map_to_msg(joint_map):
+    msg = JointState()
+
     
+    return msg
 
 if __name__ == "__main__":
     rospy.init_node('call_check_collisions', log_level=rospy.INFO)
 
     urdf_model = get_param_or_die('/robot_description')
+    srdf_model = get_param_or_die('/moveit_config')
     collision_free_config = get_param_or_die('/collision_free_config')
 
     rospy.loginfo("Read config: %s", collision_free_config)
 
     rospy.wait_for_service('/planning_scene/check_collisions')
     try:
-        check_collisions = rospy.ServiceProxy('/planning_scene/check_collisions', CollisionCheck)
-        response = check_collisions(urdf_model, JointState(), [])
-        rospy.loginfo("Detected contacts: %s", response.contacts)
+        check_collisions = rospy.ServiceProxy('/planning_scene/check_collisions', CheckCollisions)
+        response = check_collisions(urdf_model, srdf_model, joint_map_to_msg(collision_free_config), [])
+        rospy.loginfo("Detected contacts: %s", len(response.contacts))
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
     sys.exit(0)
--- knowrob_moveit_core/src/knowrob_moveit_core/planning_scene_node.cpp
+++ knowrob_moveit_core/src/knowrob_moveit_core/planning_scene_node.cpp
@@ -34,30 +34,86 @@
 #include <urdf/model.h>
 #include <knowrob_moveit_msgs/CheckCollisions.h>
 #include <moveit_msgs/PlanningScene.h>
+#include <moveit/collision_detection/collision_tools.h>
+
+void print_infos(planning_scene::PlanningScene& ps)
+{
+  std::string root_joint_type =
+      ps.getRobotModel()->getRootJoint()->getTypeName();
+  std::string root_joint_name =
+      ps.getRobotModel()->getRootJoint()->getName();
+  std::string model_frame =
+      ps.getRobotModel()->getModelFrame();
+
+  ROS_INFO("The root joint is called '%s', it is of type '%s', and the model_frame is called '%s'.", 
+      root_joint_name.c_str(), root_joint_type.c_str(), model_frame.c_str());
+}
+
+std::vector<moveit_msgs::ContactInformation> collisionResultToMsg(collision_detection::CollisionResult& collision_result)
+{
+  std::vector<moveit_msgs::ContactInformation> result;
+
+  for (collision_detection::CollisionResult::ContactMap::iterator it=collision_result.contacts.begin(); 
+       it!=collision_result.contacts.end(); ++it)
+    for (std::vector<collision_detection::Contact>::iterator it2=it->second.begin(); 
+         it2 != it->second.end(); ++it2)
+    {
+      moveit_msgs::ContactInformation msg;
+      collision_detection::contactToMsg(*it2, msg);
+      result.push_back(msg);
+    }
+
+  ROS_INFO("Finished copying contacts.");
+
+  return result;
+}
+
+std::vector<moveit_msgs::ContactInformation> calculate_collisions(planning_scene::PlanningScene& ps)
+{
+  collision_detection::CollisionRequest collision_request;
+  collision_detection::CollisionResult collision_result;
+  collision_request.contacts = true;
+  collision_request.max_contacts = 10;
+  collision_result.clear();
+  ps.checkCollision(collision_request, collision_result);
+
+  ROS_INFO("KnowRob-MoveIt check_collisions finished.");
+
+  return collisionResultToMsg(collision_result);
+}
 
 bool callback(knowrob_moveit_msgs::CheckCollisions::Request& request, 
     knowrob_moveit_msgs::CheckCollisions::Response& response)
 {
-  ROS_DEBUG("KnowRob-MoveIt check_collisions called.");
+  ROS_INFO("KnowRob-MoveIt check_collisions called.");
 
   boost::shared_ptr<urdf::Model> urdf_ptr(new urdf::Model());
   if (!urdf_ptr->initString(request.urdf_model))
   {
-    ROS_ERROR("[check_collisions] Could not parse given urdf. Aborting.");
+    ROS_ERROR("Could not parse given urdf. Aborting.");
     return false;
   }
 
-  boost::shared_ptr<const srdf::Model> srdf_ptr(new srdf::Model());
+  boost::shared_ptr<srdf::Model> srdf_ptr(new srdf::Model());
+  if (!srdf_ptr->initString(*urdf_ptr, request.srdf_model))
+  {
+    ROS_ERROR("Could not parse given srdf. Aborting.");
+    return false;
+  }
+
+  boost::shared_ptr<const srdf::Model> srdf_const_ptr = srdf_ptr;
   boost::shared_ptr<const urdf::Model> urdf_const_ptr = urdf_ptr;
 
-  planning_scene::PlanningScene ps(urdf_const_ptr, srdf_ptr);
+  planning_scene::PlanningScene ps(urdf_const_ptr, srdf_const_ptr);
 
   moveit_msgs::PlanningScene diff_ps_msg;
   diff_ps_msg.is_diff = true;
   diff_ps_msg.robot_state.joint_state = request.joint_states;
   ps.setPlanningSceneDiffMsg(diff_ps_msg);
 
-  ROS_DEBUG("KnowRob-MoveIt check_collisions finished.");
+  response.contacts = calculate_collisions(ps);
+
+  print_infos(ps);
 
   return true;
 }
