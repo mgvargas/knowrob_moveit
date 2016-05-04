117fd354c5601d9fe9cd00574971d12d416d12d6
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2016-05-04 16:50:01 +0200

    Optimized collision checking by cashing planning scene.

--- knowrob_moveit_core/src/knowrob_moveit_core/planning_scene_node.cpp
+++ knowrob_moveit_core/src/knowrob_moveit_core/planning_scene_node.cpp
@@ -31,23 +31,11 @@
 #include <ros/ros.h>
 #include <knowrob_moveit_core/knowrob_moveit_core.hpp>
 #include <string>
+#include <functional>
 #include <urdf/model.h>
 #include <knowrob_moveit_msgs/CheckCollisions.h>
 #include <moveit_msgs/PlanningScene.h>
 #include <moveit/collision_detection/collision_tools.h>
-
-void print_infos(planning_scene::PlanningScene& ps)
-{
-  std::string root_joint_type =
-      ps.getRobotModel()->getRootJoint()->getTypeName();
-  std::string root_joint_name =
-      ps.getRobotModel()->getRootJoint()->getName();
-  std::string model_frame =
-      ps.getRobotModel()->getModelFrame();
-
-  ROS_INFO("The root joint is called '%s', it is of type '%s', and the model_frame is called '%s'.", 
-      root_joint_name.c_str(), root_joint_type.c_str(), model_frame.c_str());
-}
 
 std::vector<moveit_msgs::ContactInformation> collisionResultToMsg(collision_detection::CollisionResult& collision_result)
 {
@@ -68,67 +56,120 @@ std::vector<moveit_msgs::ContactInformation> collisionResultToMsg(collision_dete
   return result;
 }
 
-std::vector<moveit_msgs::ContactInformation> calculate_collisions(planning_scene::PlanningScene& ps)
+
+
+class PlanningSceneService
 {
-  ROS_INFO("KnowRob-MoveIt check_collisions started.");
+  public:
 
-  collision_detection::CollisionRequest collision_request;
-  collision_detection::CollisionResult collision_result;
-  collision_request.contacts = true;
-  collision_request.max_contacts = 10;
-  collision_result.clear();
-  ps.checkCollision(collision_request, collision_result);
+    PlanningSceneService(const ros::NodeHandle& nh) : nh_( nh )
+    {}
+  
+    ~PlanningSceneService() {}
 
-  ROS_INFO("KnowRob-MoveIt check_collisions finished.");
+    void start()
+    {
+      service_ = nh_.advertiseService("check_collisions", &PlanningSceneService::callback, this);
+    }
 
-  return collisionResultToMsg(collision_result);
-}
+  private:
+    ros::NodeHandle nh_;
+    ros::ServiceServer service_;
+    size_t urdf_hash_, srdf_hash_;
 
-bool callback(knowrob_moveit_msgs::CheckCollisions::Request& request, 
-    knowrob_moveit_msgs::CheckCollisions::Response& response)
-{
-  ROS_INFO("KnowRob-MoveIt check_collisions called.");
+    planning_scene::PlanningScenePtr ps_ptr_;
 
-  boost::shared_ptr<urdf::Model> urdf_ptr(new urdf::Model());
-  if (!urdf_ptr->initString(request.urdf_model))
-  {
-    ROS_ERROR("Could not parse given urdf. Aborting.");
-    return false;
-  }
+    void createPlanningScene(const std::string& urdf, const std::string& srdf)
+    {
+      ROS_INFO("Hashing started.");
+      std::hash<std::string> hash_fn;
+      size_t new_urdf_hash = hash_fn(urdf);
+      size_t new_srdf_hash = hash_fn(srdf);
+      ROS_INFO("Hashing done.");
 
-  boost::shared_ptr<srdf::Model> srdf_ptr(new srdf::Model());
-  if (!srdf_ptr->initString(*urdf_ptr, request.srdf_model))
-  {
-    ROS_ERROR("Could not parse given srdf. Aborting.");
-    return false;
-  }
+      if(!ps_ptr_ || urdf_hash_ != new_urdf_hash || srdf_hash_ != new_srdf_hash)
+      {
+        ROS_INFO("Parsing urdf started.");
+        boost::shared_ptr<urdf::Model> urdf_ptr(new urdf::Model());
+        if (!urdf_ptr->initString(urdf))
+          throw std::runtime_error("Could not parse given urdf. Aborting.");
+        ROS_INFO("Parsing urdf done.");
 
-  boost::shared_ptr<const srdf::Model> srdf_const_ptr = srdf_ptr;
-  boost::shared_ptr<const urdf::Model> urdf_const_ptr = urdf_ptr;
+        ROS_INFO("Parsing srdf started.");
+        boost::shared_ptr<srdf::Model> srdf_ptr(new srdf::Model());
+        if (!srdf_ptr->initString(*urdf_ptr, srdf))
+          throw std::runtime_error("Could not parse given srdf. Aborting.");
+        ROS_INFO("Parsing srdf done.");
+   
+        ROS_INFO("Creation planning scene started.");
+        boost::shared_ptr<const srdf::Model> srdf_const_ptr = srdf_ptr;
+        boost::shared_ptr<const urdf::Model> urdf_const_ptr = urdf_ptr;
+    
+        ps_ptr_ = planning_scene::PlanningScenePtr(new planning_scene::PlanningScene(urdf_const_ptr, srdf_const_ptr));
+        urdf_hash_ = new_urdf_hash;
+        srdf_hash_ = new_srdf_hash;
+        ROS_INFO("Creation planning scene done.");
+      }
+    }
 
-  planning_scene::PlanningScene ps(urdf_const_ptr, srdf_const_ptr);
+    std::vector<moveit_msgs::ContactInformation> calculate_collisions()
+    {
+      ROS_INFO("KnowRob-MoveIt check_collisions started.");
+    
+      collision_detection::CollisionRequest collision_request;
+      collision_detection::CollisionResult collision_result;
+      collision_request.contacts = true;
+      collision_request.max_contacts = 10;
+      collision_result.clear();
+      ps_ptr_->checkCollision(collision_request, collision_result);
+    
+      ROS_INFO("KnowRob-MoveIt check_collisions finished.");
+    
+      return collisionResultToMsg(collision_result);
+    }
+    bool callback(knowrob_moveit_msgs::CheckCollisions::Request& request, 
+        knowrob_moveit_msgs::CheckCollisions::Response& response)
+    {
+      ROS_INFO("KnowRob-MoveIt check_collisions called.");
+    
+      try{
+        createPlanningScene(request.urdf_model, request.srdf_model);
+      }
+      catch(const std::exception& e)
+      {
+        ROS_ERROR("%s", e.what());
+        return false;
+      }
 
-  moveit_msgs::PlanningScene diff_ps_msg;
-  diff_ps_msg.is_diff = true;
-  diff_ps_msg.robot_state.joint_state = request.joint_states;
-  ps.setPlanningSceneDiffMsg(diff_ps_msg);
+      ROS_INFO("Update planning scene started.");
+      moveit_msgs::PlanningScene diff_ps_msg;
+      diff_ps_msg.is_diff = true;
+      diff_ps_msg.robot_state.joint_state = request.joint_states;
+      ps_ptr_->setPlanningSceneDiffMsg(diff_ps_msg);
+      ROS_INFO("Update planning scene done.");
+   
+      response.contacts = calculate_collisions();
+    
+      return true;
+    }
 
-  response.contacts = calculate_collisions(ps);
-
-  print_infos(ps);
-
-  return true;
-}
-
+};
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "planning_scene");
   ros::NodeHandle nh("~");
 
-  ros::ServiceServer service = nh.advertiseService("check_collisions", callback);
-
-  ROS_DEBUG("KnowRob-MoveIt planning scene up.");
+  PlanningSceneService psserver(nh);
+  try
+  {
+    psserver.start();
+  }
+  catch(const std::exception& e)
+  {
+    ROS_ERROR("%s", e.what());
+    return 0;
+  }
 
   ros::spin();
 
