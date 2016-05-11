3a8f06fc33c8fe92c8dd69ad801965a0875725a9
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2016-05-11 09:17:49 +0200

    Bug-fix planning_scene_client: Keep reference to joint-state subscriber.

--- src/knowrob_moveit/planning_scene_client.cpp
+++ src/knowrob_moveit/planning_scene_client.cpp
@@ -28,7 +28,9 @@ class PlanningSceneClient
      robot_description_ = readParam(nh_, "/robot_description");
      moveit_config_ = readParam(nh_, "/moveit_config");
      collision_pub_ = nh_.advertise<knowrob_moveit::ContactList>("contacts", 1);
-     nh_.subscribe("/joint_states", 1, &PlanningSceneClient::callback, this);
+     // TODO: get rid of debug publisher when done with this
+     debug_pub_ = nh_.advertise<sensor_msgs::JointState>("debug_topic", 1);
+     js_sub_ = nh_.subscribe("/joint_states", 1, &PlanningSceneClient::callback, this);
      collision_service_ = nh_.serviceClient<knowrob_moveit::CheckCollisions>("/planning_scene_server/check_collisions");
      trigger_service_ = nh_.advertiseService("trigger", &PlanningSceneClient::trigger_callback, this);
     }
@@ -37,6 +39,8 @@ class PlanningSceneClient
     ros::NodeHandle nh_;
     ros::Subscriber js_sub_;
     ros::Publisher collision_pub_;
+    // TODO: get rid of debug publisher when done with this
+    ros::Publisher debug_pub_;
     ros::ServiceServer trigger_service_;
     ros::ServiceClient collision_service_;
     sensor_msgs::JointState joint_states_;
@@ -50,6 +54,8 @@ class PlanningSceneClient
 
     bool trigger_callback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
     {
+      debug_pub_.publish(joint_states_);
+
       knowrob_moveit::CheckCollisions srv;
       srv.request.urdf_model = robot_description_;
       srv.request.srdf_model = moveit_config_;
