2681081df3c2a8f6c704c010bb39c9193b7f7ad7
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2016-05-11 09:09:22 +0200

    Have planning_scene_client publish a list of contacts for debugging and visualization purposes.

--- src/knowrob_moveit/planning_scene_client.cpp
+++ src/knowrob_moveit/planning_scene_client.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
-#include <knowrob_moveit_msgs/CheckCollisions.h>
+#include <knowrob_moveit/CheckCollisions.h>
+#include <knowrob_moveit/ContactList.h>
 #include <sensor_msgs/JointState.h>
 #include <std_srvs/Empty.h>
 #include <stdexcept>
@@ -26,14 +27,16 @@ class PlanningSceneClient
     {
      robot_description_ = readParam(nh_, "/robot_description");
      moveit_config_ = readParam(nh_, "/moveit_config");
+     collision_pub_ = nh_.advertise<knowrob_moveit::ContactList>("contacts", 1);
      nh_.subscribe("/joint_states", 1, &PlanningSceneClient::callback, this);
-     collision_service_ = nh_.serviceClient<knowrob_moveit_msgs::CheckCollisions>("/planning_scene_server/check_collisions");
+     collision_service_ = nh_.serviceClient<knowrob_moveit::CheckCollisions>("/planning_scene_server/check_collisions");
      trigger_service_ = nh_.advertiseService("trigger", &PlanningSceneClient::trigger_callback, this);
     }
 
   private:
     ros::NodeHandle nh_;
     ros::Subscriber js_sub_;
+    ros::Publisher collision_pub_;
     ros::ServiceServer trigger_service_;
     ros::ServiceClient collision_service_;
     sensor_msgs::JointState joint_states_;
@@ -47,13 +50,16 @@ class PlanningSceneClient
 
     bool trigger_callback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
     {
-      knowrob_moveit_msgs::CheckCollisions srv;
+      knowrob_moveit::CheckCollisions srv;
       srv.request.urdf_model = robot_description_;
       srv.request.srdf_model = moveit_config_;
       srv.request.joint_states = joint_states_;
      
       if (collision_service_.call(srv))
       {
+        knowrob_moveit::ContactList msg;
+        msg.contacts = srv.response.contacts;
+        collision_pub_.publish(msg);
         ROS_INFO("Detected %lu collisions.", srv.response.contacts.size());
         return true;
       }
