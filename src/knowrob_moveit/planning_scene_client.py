583a1acdc80692f9c034da1ce59342dbadfb7a52
Minerva Vargas <minervavargasg@gmail.com>
2017-06-13 15:57:04 +0200

    Not working

--- src/knowrob_moveit/planning_scene_client.py
+++ src/knowrob_moveit/planning_scene_client.py
@@ -1,8 +1,19 @@
 import rospy
-import copy
+import moveit_commander
+import rospkg
+import xml.etree.ElementTree as element_tree
 from knowrob_moveit.msg import ContactList
 from sensor_msgs.msg import JointState
+from std_msgs.msg import Header
+from urdf_parser_py.urdf import URDF
 from knowrob_moveit.srv import *
+
+
+def make_header(stamp,frame_id):
+    msg = Header()
+    msg.stamp = stamp
+    msg.frame_id = frame_id
+    return msg
 
 
 def read_param(nh, parameter_name):
@@ -11,6 +22,7 @@ def read_param(nh, parameter_name):
                      format(parameter_name, rospy.get_namespace()))
     result = rospy.get_param(parameter_name, nh)
     return result
+
 
 class PlanningSceneClient:
     def __init__(self):
@@ -26,6 +38,13 @@ class PlanningSceneClient:
         except rospy.ServiceException, e:
             print "Service call failed: %s" % e
         self.trigger_service_ = rospy.Service('trigger', UInt64Trigger, self.trigger_callback)
+
+        # From header
+        moveit_commander.roscpp_initialize(sys.argv)
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.urdf_hash_ = None
+        self.srdf_hash_ = None
+        self.ps_ptr_ = None
 
     def callback(self, message):
         self.joint_states_ = message.data
@@ -43,12 +62,49 @@ class PlanningSceneClient:
             msg.contacts = response.contacts
             self.collision_pub_.publish(msg)
             response.success = True
-            response.message = "Detected " + str(len(srv.size())) + " collisions."
+            response.message = "Detected " + str(len(response.contacts)) + " collisions."
             return True
         else:
             response.success = False
             response.message = "Service call to planning scene failed."
             return False
+
+    # From header file
+    def check_collisions(self, urdf_model, srdf_model, joint_states, max_contacts):
+        self.create_planning_scene(urdf_model, srdf_model)
+        return self.calculate_collisions(max_contacts, joint_states)
+
+    def create_planning_scene(self, urdf, srdf):
+        rospy.loginfo("Hashing started.")
+        new_urdf_hash = hash(urdf)
+        new_srdf_hash = hash(srdf)
+        rospy.loginfo("Hashing done.")
+        if not self.ps_ptr_ or self.urdf_hash_ != new_urdf_hash or self.srdf_hash_ != new_srdf_hash:
+            rospy.loginfo("Parsing urdf started.")
+            rospack = rospkg.RosPack()
+            dir = rospack.get_path('knowrob_moveit') + '/test_data/boxy.urdf'
+            try:
+                # self.urdf_ptr = urdf.Robot.from_xml_file(dir)
+                # self.urdf_ptr = URDF.from_parameter_server()
+                self.urdf_ptr = element_tree.parse(urdf)
+            except (OSError, LookupError) as error:
+                rospy.logerr('Could not parse given urdf. Aborting.')
+                print 'ERROR: ', error
+            rospy.loginfo("Parsing urdf done.")
+
+            rospy.loginfo("Parsing srdf started.")
+            try:
+                self.srdf_ptr = element_tree.parse(srdf)
+            except (OSError, LookupError) as error:
+                rospy.logerr('Could not parse given srdf. Aborting.')
+                print 'ERROR: ', error
+            rospy.loginfo("Parsing srdf done.")
+
+            rospy.loginfo("Creation planning scene started.")
+
+            self.urdf_hash_ = new_urdf_hash
+            self.srdf_hash_ = new_srdf_hash
+            rospy.loginfo("Creation planning scene done.")
 
 
 def main():
